a9293d3079c7f8b3c8e9c4a9c4ca354a31efdf41
Angela-Shtangey <cnegurka@gmail.com>
2021-11-18 21:37:20 +0200

    hm1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict';
+// eslint-disable-next-line prefer-const
 let productName = 'Кресло';
+// eslint-disable-next-line prefer-const
 let productPrice = 12000;
+// eslint-disable-next-line prefer-const
 let productString = `${productName}, цена ${productPrice} рублей.`;
 alert(productString);
-
