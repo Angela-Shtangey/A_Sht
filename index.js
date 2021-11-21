cee431d68903e903c6af2c4a2ee97ed505a37d62
Angela-Shtangey <Angela-Shtangey@users.noreply.github.com>
2021-11-21 09:42:15 +0000

    Deploying to gh-pages from @ Angela-Shtangey/A_Sht@8cb440feaa8b2a2de0c27315744158acfd6926d4 🚀

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ let productPrice = 12000;
 // eslint-disable-next-line prefer-const
 let productString = `${productName}, цена ${productPrice} рублей.`;
 alert(productString);
+// eslint-disable-next-line prefer-const
+let first = prompt('Введите первое значение');
+// eslint-disable-next-line prefer-const
+let second = prompt('Введите второе значение');
+function RandomGenerate (first, second) {
+  return Math.round(+first + Math.random() * (second - first));
+// унарный плюс перед first преобразовывает переданный стринг в int
+// также сразу решена проблема когда min=max
+// и нет разницы какое число пользователь считает минимальным, какое максимальным
+}
+// eslint-disable-next-line
+console.log(RandomGenerate (first, second));
